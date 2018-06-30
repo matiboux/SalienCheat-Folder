4cc5819804c9eefca70f8e24d71a205209399607
hexicans <Axel@dyjix.eu>
2018-06-30 16:53:35 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -6,4 +6,5 @@ unzip master.zip >/dev/null 2>&1
 rm master.zip >/dev/null 2>&1
 cp token.txt SalienCheat-master/token.txt >/dev/null 2>&1
 echo "Script successfully updated."
+chmod +x salien-run.sh
 sh salien-run.sh
