10b5b486383dda1c8b8cbb2197f5a5897cdce4a8
hexicans <Axel@dyjix.eu>
2018-06-30 14:31:01 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
-rm SalienCheat-master/ -r
-wget https://github.com/SteamDatabase/SalienCheat/archive/master.zip
-unzip master.zip
-rm master.zip
-cp token.txt SalienCheat-master/token.txt
+#!/bin/bash
+
+rm -rf SalienCheat-master/
+wget https://github.com/SteamDatabase/SalienCheat/archive/master.zip >/dev/null 2>&1
+unzip master.zip >/dev/null 2>&1
+rm master.zip >/dev/null 2>&1
+cp token.txt SalienCheat-master/token.txt >/dev/null 2>&1
 sh salien.sh
