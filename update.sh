4da1b758c6d7cc79c3116ec20a96761b702eecc4
Matiboux <matiboux@gmail.com>
2018-06-30 14:58:24 +0200

    Added displayed message after update

--- update.sh
+++ update.sh
@@ -5,4 +5,5 @@ wget https://github.com/SteamDatabase/SalienCheat/archive/master.zip >/dev/null 
 unzip master.zip >/dev/null 2>&1
 rm master.zip >/dev/null 2>&1
 cp token.txt SalienCheat-master/token.txt >/dev/null 2>&1
+echo "Script successfully updated."
 sh salien-run.sh
